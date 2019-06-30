17f4c9b75e544c507fadbb1aa286d2f810499210
lgc_cn <lgc_cn@163.com>
2019-06-30 21:00:42 +0800

    config配置中心

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 在spring cloud config 组件中，分两个角色，一是config server，二是config client。
+ * 启动程序：访问http://localhost:8888/foo/dev,若能得到数据，证明配置服务中心可以从远程程序获取配置信息
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
