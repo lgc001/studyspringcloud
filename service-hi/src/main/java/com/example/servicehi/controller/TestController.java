7540c2673d0198dd95c94f97e3529a933bc334f8
lgc_cn <lgc_cn@163.com>
2019-06-30 22:40:40 +0800

    config配置中心

--- service-hi/src/main/java/com/example/servicehi/controller/TestController.java
+++ service-hi/src/main/java/com/example/servicehi/controller/TestController.java
@@ -9,6 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    //这里是调用配置中心，启动不了，似乎是不能既读本项目配置也读配置中心。config-client是只能配置中心
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/readConfigFoo")
+    public String readConfigFoo(){
+        return foo;
+    }
+
     @RequestMapping("/index")
     public String index() {
 
